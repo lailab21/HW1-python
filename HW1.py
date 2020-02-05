d403f53df4b64ead8d4ea5ef9dcdbc7f4bdb9cf7
Laila Barradouane <LBarradouane@spartaglobal.com>
2020-02-05 09:13:27 +0000

    third commit

--- HW1.py
+++ HW1.py
@@ -4,12 +4,12 @@ age = '22'
 eye_colour = 'brown'
 hair_colour = 'brown'
 
-new_name = print(f'Hello {name}!What new name would you like?')
+name = input(f'Hello {name}!What new name would you like?')
 
 print(f'Hello {name}! Welcome, your age is {age}, your eyes are {eye_colour}, and your hair colour is {hair_colour}')
 
-name = 'laila'
+new_name = 'laila'
 current_year = 2020
 calculate_age = int(input(f'Hello {name}, how old are you?'))
 birth_year = current_year - calculate_age
-print(f'You said you were {calculate_age} so you were born in {birth_year}')
+print(f'Hello! {new_name}!You said you were {calculate_age} so you were born in {birth_year}')
